833abf377618e6e4b8526e3da77f008da5f9a0b5
Brian Lee <leebrian0908@gmail.com>
2025-10-21 21:44:34 +0800

    Initial Commit

---
+++ Eco-craft Trail Guidebook/Eco_craft_Trail_GuidebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eco_craft_Trail_GuidebookApp.swift
+//  Eco-craft Trail Guidebook
+//
+//  Created by Brian Lee on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Eco_craft_Trail_GuidebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
