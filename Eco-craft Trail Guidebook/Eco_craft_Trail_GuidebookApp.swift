33af9ba86caed40bcbbb2930e05a428183ee2aa7
Brian Lee <leebrian0908@gmail.com>
2025-10-23 21:35:06 +0800

    📚 Update - 2025-10-23 21:35:06

--- Eco-craft Trail Guidebook/Eco_craft_Trail_GuidebookApp.swift
+++ Eco-craft Trail Guidebook/Eco_craft_Trail_GuidebookApp.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 @main
 struct Eco_craft_Trail_GuidebookApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	init() {
+		let ap = UINavigationBarAppearance()
+		ap.configureWithTransparentBackground()
+		ap.backgroundEffect = nil
+		ap.backgroundColor = .clear
+		ap.shadowColor = .clear
+		ap.titleTextAttributes      = [.foregroundColor: UIColor.black,
+									   .font: UIFont.boldSystemFont(ofSize: 17)]
+		ap.largeTitleTextAttributes = [.foregroundColor: UIColor.black,
+									   .font: UIFont.boldSystemFont(ofSize: 34)]
+	}
+
+	var body: some Scene {
+		WindowGroup { ContentView() }
+	}
 }
